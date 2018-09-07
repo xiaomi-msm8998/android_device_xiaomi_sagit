f7d75e0a3faf3851d0d4d09a0c0721b6eb5c8f1a
arian <arian.kulmer@web.de>
2018-09-07 18:02:21 +0200

    sagit: say welcome to aex pie

--- aosp_sagit.mk
+++ aosp_sagit.mk
@@ -18,13 +18,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit some common AospExtended stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
 # Inherit from sagit device
 $(call inherit-product, device/xiaomi/sagit/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-PRODUCT_NAME := lineage_sagit
+PRODUCT_NAME := aosp_sagit
 PRODUCT_DEVICE := sagit
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 6
