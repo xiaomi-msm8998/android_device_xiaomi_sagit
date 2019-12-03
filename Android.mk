df10c96c5916471fa7ef080f793b586c7da7e207
Arian <arian.kulmer@web.de>
2019-12-03 09:53:21 +0100

    sagit: Hack fpc fingerprint HAL for new persist mount point
    Change-Id: I7c12a4eab78639f8725387b8cddbb8f82b8f3c81

--- Android.mk
+++ Android.mk
@@ -20,4 +20,15 @@ ifeq ($(TARGET_DEVICE),sagit)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+include $(CLEAR_VARS)
+
+PERSIST_FPCALIBRATION_SYMLINK := $(TARGET_OUT_VENDOR)/etc/fp/calibrationimage.pndat
+$(PERSIST_FPCALIBRATION_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "FPC Calibrationimage link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /mnt/vendor/persist/misc/calibration_image.pndat $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(PERSIST_FPCALIBRATION_SYMLINK)
+
 endif
