238244b861fd0ed0906d3acbaadc6827e9f84b11
Arian <arian.kulmer@web.de>
2020-03-09 19:44:20 +0100

    sagit: overlay: Define fingerprint sensor location
    Change-Id: I7b61ada1c926c621f8fffeab468f17e8ba4931f3

--- device.mk
+++ device.mk
@@ -18,7 +18,10 @@
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-pa \
+    $(LOCAL_PATH)/overlay
+
 
 # Inherit from msm8998-common
 $(call inherit-product, device/xiaomi/msm8998-common/msm8998.mk)
